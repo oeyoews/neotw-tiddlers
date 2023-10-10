cea5b2ca447ee4ee56cd5379e5ec785832dfc1ae
oeyoews <jyao4783@gmail.com>
2023-10-10 18:57:56 +0800

    update tagpie

--- content/tagpie.js
+++ content/tagpie.js
@@ -5,16 +5,11 @@ type: text/application
 description: tag pie on tiddlywiki
 \*/
 
-// TODO: group legend scroll type https://echarts.apache.org/examples/en/editor.html?c=pie-legend
-// 生命周期不随着widget变化???
-// TODO: 生命周期
 // TODO: refresh
-// TODO: 如何暴露参数给echarts
-// tags: default is all, 支持指定tag
-// TODO: 定义事件
+// TODO: 如何暴露参数给echarts, default is all, 支持指定tag
 // TODO: tag排序
 // TOOD: 封装成插件
-// TODO: percent
+// TODO: https://github.com/tiddly-gittly/tw-echarts/blob/master/src/echarts/addons/Gk0Wk/TagCloud/TagCloud.ts 可以将事件直接放在addon里面
 
 const data = [];
 
@@ -26,16 +21,16 @@ function getData(tag) {
 	}
 }
 
-// const tags = $tw.wiki.filterTiddlers('[tags[]!prefix[$:/]]')
-const tags = ['JavaScript', 'CSS', 'Journal', 'React', 'Videos', 'TiddlyWiki', 'Tailwindcss', 'books']
+const tags = $tw.wiki.filterTiddlers('[tags[]!prefix[$:/]]')
+// const tags = ['JavaScript', 'CSS', 'Journal', 'React', 'Videos', 'TiddlyWiki', 'Tailwindcss', 'books']
 
 tags.forEach(tag => data.push(getData(tag)))
 
 
 const option = {
 	title: {
-		text: '文章类型分布',
-		subtext: '',
+		text: '文章标签占比分布',
+		subtext: '用户标签',
 		left: 'center'
 	},
 	tooltip: {
@@ -47,6 +42,7 @@ const option = {
 		right: 10,
 		top: 20,
 		bottom: 20,
+		type: 'scroll'
 	},
 	series: [
 		{
@@ -67,7 +63,6 @@ const option = {
 };
 
 function onMount(echartsInstance) {
-	echartsInstance.setOption(option)
 }
 
 // TODO: 监听对应的tags tiddler, 更新
@@ -76,10 +71,11 @@ function shouldUpdate(state, changedTiddlers, changedAttributes) {
 }
 
 function onUpdate(echartsInstance) {
+	echartsInstance.setOption(option)
 }
 
 function onUnmount(state) {
-
+  
 }
 
 // https://tiddly-gittly.github.io/tw-echarts/#%E6%88%91%E8%AF%A5%E5%A6%82%E4%BD%95%E4%BD%BF%E7%94%A8%E8%AF%A5%E6%8F%92%E4%BB%B6%3F
--- content/tagpie.md
+++ content/tagpie.md
@@ -1,1 +1,1 @@
-<$echarts $tiddler="tagpie.js" />
+<$echarts $tiddler="tagpie.js" $theme="light" dblclick="(params, parentWidget) => { parentWidget.dispatchEvent({ type: 'tm-navigate', navigateTo: params.data.name }) }"/>
