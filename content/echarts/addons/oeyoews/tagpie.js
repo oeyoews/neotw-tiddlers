227dc4bbbc6c97872b5937bb1bde563183f3077f
oeyoews <jyao4783@gmail.com>
2023-10-12 09:09:54 +0800

    format: use vscode prettier to format code

--- content/echarts/addons/oeyoews/tagpie.js
+++ content/echarts/addons/oeyoews/tagpie.js
@@ -7,117 +7,123 @@ description: tag pie on tiddlywiki
 \*/
 
 function getData(tag) {
-	const count = $tw.wiki.filterTiddlers(`[tag[${tag}]!has[draft.of]]`).length
-	return {
-		name: tag,
-		value: count
-	}
+  const count = $tw.wiki.filterTiddlers(`[tag[${tag}]!has[draft.of]]`).length;
+  return {
+    name: tag,
+    value: count,
+  };
 }
-
 
 const goto = new $tw.Story();
 
 const gotoTagTiddler = (params) => {
-	const title = params.name
-	const existTiddler = $tw.wiki.tiddlerExists(title)
-	if (!existTiddler) {
-		console.log(title + ' not found')
-		return;
-	}
-	// parentWidget.dispatchEvent( { type: 'tm-navigate', navigateTo: title } )
-	goto.navigateTiddler(title);
-}
+  const title = params.name;
+  const existTiddler = $tw.wiki.tiddlerExists(title);
+  if (!existTiddler) {
+    console.log(title + " not found");
+    return;
+  }
+  // parentWidget.dispatchEvent( { type: 'tm-navigate', navigateTo: title } )
+  goto.navigateTiddler(title);
+};
 
 function onUpdate(myChart, _, addonAttributes) {
-	// 暴露参数给用户
-	const { doughnut, filter='[tags[]!prefix[$:/]]', sort="descend" } = addonAttributes;
-	// data必须在执行onUpdate函数的时候获取到最新数据,不要写在函数外面
-	const data = [];
-	// 配置具体参考echarts官方文档
-	const option = {
-		title: {
-			text: '文章标签占比分布',
-			subtext: '用户标签',
-			left: 'center',
-			top: "bottom",
-		},
-		toolbox: {
-			show: true,
-			left: 0,
-			bottom: 0,
-			feature: {
+  // 暴露参数给用户
+  const {
+    doughnut,
+    filter = "[tags[]!prefix[$:/]]",
+    sort = "descend",
+  } = addonAttributes;
+  // data必须在执行onUpdate函数的时候获取到最新数据,不要写在函数外面
+  const data = [];
+  // 配置具体参考echarts官方文档
+  const option = {
+    title: {
+      text: "文章标签占比分布",
+      subtext: "用户标签",
+      left: "center",
+      top: "bottom",
+    },
+    toolbox: {
+      show: true,
+      left: 0,
+      bottom: 0,
+      feature: {
         dataView: { show: true, readOnly: false },
-				restore: {},
-				saveAsImage: {},
-			},
-		},
-		tooltip: {
-			trigger: 'item',
-			formatter: function (params) {
-				const {name, value, percent} = params;
-				if(value) {
-					return `${name} 标签 有 ${value}个条目, ${percent}%`;
-				} else {
-					return name;
-				}
-			}
-		},
-		legend: {
-			orient: 'vertical',
-			right: 10,
-			top: 20,
-			bottom: 20,
-			type: 'scroll'
-		},
-		series: [
-			{
-				name: 'Tag',
-				type: 'pie',
-				radius: doughnut === "yes" ? ['40%', '70%'] : "50%",
-				center: ['40%', '50%'],
-				data,
-	      itemStyle: {
-					borderRadius: 10,
-					borderWidth: 0,
-					borderColor: '#fff',
+        restore: {},
+        saveAsImage: {},
       },
-				emphasis: {
-					itemStyle: {}
-				}
-			}
-		]
-	};
+    },
+    tooltip: {
+      trigger: "item",
+      formatter: function (params) {
+        const { name, value, percent } = params;
+        if (value) {
+          return `${name} 标签 有 ${value}个条目, ${percent}%`;
+        } else {
+          return name;
+        }
+      },
+    },
+    legend: {
+      orient: "vertical",
+      right: 10,
+      top: 20,
+      bottom: 20,
+      type: "scroll",
+    },
+    series: [
+      {
+        name: "Tag",
+        type: "pie",
+        radius: doughnut === "yes" ? ["40%", "70%"] : "50%",
+        center: ["40%", "50%"],
+        data,
+        itemStyle: {
+          borderRadius: 10,
+          borderWidth: 0,
+          borderColor: "#fff",
+        },
+        emphasis: {
+          itemStyle: {},
+        },
+      },
+    ],
+  };
 
-	// alpha sort default
-  const tags =  $tw.wiki.filterTiddlers(filter).sort()
-	tags.forEach(tag => data.push(getData(tag)))
-	
-	// descend or ascend sort
-	data.sort(function(a,b) {
-		return sort === "descend" ? b.value - a.value : a.value - b.value;
-	})
-	
-	myChart.setOption(option)
-	myChart.on('click', 'series', gotoTagTiddler)
+  // alpha sort default
+  const tags = $tw.wiki.filterTiddlers(filter).sort();
+  tags.forEach((tag) => data.push(getData(tag)));
+
+  // descend or ascend sort
+  data.sort(function (a, b) {
+    return sort === "descend" ? b.value - a.value : a.value - b.value;
+  });
+
+  myChart.setOption(option);
+  myChart.on("click", "series", gotoTagTiddler);
 }
 
 // TODO: need refresh manually here
 function shouldUpdate(_, changedTiddlers) {
-	// changeTiddlers 会包含一些系统tiddler的状态变换tiddler, 应该去掉
-	const changedTiddlersLength = Object.keys(changedTiddlers).filter(
-		tiddler => !(tiddler.startsWith('$:/') || tiddler.startsWith('Draft of')).length);
-	return changedTiddlersLength ? true : false;
+  // changeTiddlers 会包含一些系统tiddler的状态变换tiddler, 应该去掉
+  const changedTiddlersLength = Object.keys(changedTiddlers).filter(
+    (tiddler) =>
+      !(tiddler.startsWith("$:/") || tiddler.startsWith("Draft of")).length
+  );
+  return changedTiddlersLength ? true : false;
 }
 
 // function onMount() { }
 // function onUnmount() {}
 
 module.exports = {
-	onUpdate, shouldUpdate
-}
+  onUpdate,
+  shouldUpdate,
+};
 
-/**
-  * @param: filter, 默认是用户的所有tiddler, 但是你也可以使用 filter='[tag[Journal]]' 列出所有的 Journal tiddler
-  * @param: sort {descend|ascend}
-	* @param: doughnut {'yes'}
-  */
+/*
+ * @param: filter, 默认是用户的所有tiddler, 但是你也可以使用 filter='[tag[Journal]]' 列出所有的 Journal tiddler
+ * @param: sort {descend|ascend}
+ * @param: doughnut {'yes'}
+ */
--- content/sevenday.js
+++ content/sevenday.js
@@ -6,108 +6,116 @@ description: seven
 \*/
 
 // TODO: 简化时间的处理
-const getData = (date) => $tw.wiki.filterTiddlers(`[sameday:created[${date}]!is[system]!has[draft.of]]`).length
+const getData = (date) =>
+  $tw.wiki.filterTiddlers(`[sameday:created[${date}]!is[system]!has[draft.of]]`)
+    .length;
 
 function parsesixDate(dateString) {
-		const year = parseInt(dateString.substr(0, 4));
-		const month = parseInt(dateString.substr(4, 2)) - 1; // 月份从0开始，需要减1
-		const day = parseInt(dateString.substr(6, 2));
-	const realDate = new Date(year, month, day).toLocaleDateString();
-	return realDate;
+  const year = parseInt(dateString.substr(0, 4));
+  const month = parseInt(dateString.substr(4, 2)) - 1; // 月份从0开始，需要减1
+  const day = parseInt(dateString.substr(6, 2));
+  const realDate = new Date(year, month, day).toLocaleDateString();
+  return realDate;
 }
 
 function getSevenDaysBefore(dateString) {
-	const currentDate = dateString ? parsesixDate(dateString) : new Date()
+  const currentDate = dateString ? parsesixDate(dateString) : new Date();
 
-	const sevenDays = [];
+  const sevenDays = [];
 
-	for (let i = 0; i < 7; i++) {
-		// 获取当前日期的年、月、日
-		const year = currentDate.getFullYear();
-		const month = currentDate.getMonth() + 1; // 月份从0开始，需要加1
-		const day = currentDate.getDate();
+  for (let i = 0; i < 7; i++) {
+    // 获取当前日期的年、月、日
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1; // 月份从0开始，需要加1
+    const day = currentDate.getDate();
 
-		// 将年、月、日格式化成字符串，并添加到数组
-		const dateString = `${year}${month < 10 ? '0' : ''}${month}${day < 10 ? '0' : ''}${day}`;
-		sevenDays.unshift(dateString); // 使用unshift方法将日期添加到数组头部
+    // 将年、月、日格式化成字符串，并添加到数组
+    const dateString = `${year}${month < 10 ? "0" : ""}${month}${
+      day < 10 ? "0" : ""
+    }${day}`;
+    sevenDays.unshift(dateString); // 使用unshift方法将日期添加到数组头部
 
-		// 将当前日期减一天，以便生成前一天的日期
-		currentDate.setDate(currentDate.getDate() - 1);
-	}
+    // 将当前日期减一天，以便生成前一天的日期
+    currentDate.setDate(currentDate.getDate() - 1);
+  }
 
-	return sevenDays;
+  return sevenDays;
 }
-
 
 // function shouldUpdate(_state, changedTiddlers, _changedAttributes) { return true; }
 
 function onUpdate(myChart, _state, addonAttributes) {
+  const {
+    date,
+    title: text = "最近文章数量",
+    subtitle: subtext = "",
+    disableClick = "no",
+  } = addonAttributes;
+  const data = [];
+  const sevendays = getSevenDaysBefore(date);
+  sevendays.forEach((date) => data.push(getData(date)));
 
-	const {date, title: text="最近文章数量", subtitle: subtext='', disableClick='no'} = addonAttributes;
-	const data = []
-	const sevendays = getSevenDaysBefore(date)
-	sevendays.forEach(date => data.push(getData(date)))
+  const option = {
+    title: {
+      text,
+      subtext,
+      left: "center",
+    },
+    tooltip: {
+      trigger: "item",
+      formatter: function (params) {
+        const { name: date, value: count } = params;
+        const realDate = parsesixDate(date);
+        return count
+          ? `${realDate} 有 ${count} 篇文章`
+          : `${realDate} 没有新的文章`;
+      },
+    },
+    // color: [''],
+    xAxis: {
+      type: "category",
+      data: sevendays,
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data,
+        type: "line",
+        emphasis: {
+          itemStyle: {
+            scale: 1.25,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+        smooth: true,
+      },
+    ],
+  };
 
-	const option = {
-		title: {
-			text,
-			subtext,
-			left: 'center'
-		},
-		tooltip: {
-			trigger: 'item',
-			formatter: function (params) {
-				const {name:date, value:count} = params;
-				const realDate = parsesixDate(date);
-				return count ? `${realDate} 有 ${count} 篇文章` : `${realDate} 没有新的文章`
-			}
-		},
-		// color: [''],
-		xAxis: {
-			type: 'category',
-			data: sevendays,
-		},
-		yAxis: {
-			type: 'value'
-		},
-		series: [
-			{
-				data,
-				type: 'line',
-				emphasis: {
-					itemStyle: {
-						scale: 1.25,
-						shadowOffsetX: 0,
-						shadowColor: 'rgba(0, 0, 0, 0.5)'
-					}
-				},
-				smooth: true
-			}
-		]
-	};
-
-	myChart.setOption(option)
-  myChart.on('click', 'series', function (params) {
-		const { name: date } = params
-		const goto = new $tw.Story();
-		const filter = `[sameday:created[${date}]!is[system]!has[draft.of]]`
-		const hasTiddler = $tw.wiki.filterTiddlers(filter).length
-		if (!hasTiddler)  return;
-		$tw.rootWidget.invokeActionString(
-			'<$action-setfield $tiddler="$:/temp/advancedsearch" text="""' +
-			filter +
-			'"""/><$action-setfield $tiddler="$:/temp/advancedsearch/input" text="""' +
-			filter +
-			'"""/><$action-setfield $tiddler="$:/temp/advancedsearch/refresh" text="yes"/><$action-setfield $tiddler="$:/state/tab--1498284803" text="$:/core/ui/AdvancedSearch/Filter"/>',
-		);
-		goto.navigateTiddler('$:/AdvancedSearch');
-
-	})
+  myChart.setOption(option);
+  myChart.on("click", "series", function (params) {
+    const { name: date } = params;
+    const goto = new $tw.Story();
+    const filter = `[sameday:created[${date}]!is[system]!has[draft.of]]`;
+    const hasTiddler = $tw.wiki.filterTiddlers(filter).length;
+    if (!hasTiddler) return;
+    $tw.rootWidget.invokeActionString(
+      '<$action-setfield $tiddler="$:/temp/advancedsearch" text="""' +
+        filter +
+        '"""/><$action-setfield $tiddler="$:/temp/advancedsearch/input" text="""' +
+        filter +
+        '"""/><$action-setfield $tiddler="$:/temp/advancedsearch/refresh" text="yes"/><$action-setfield $tiddler="$:/state/tab--1498284803" text="$:/core/ui/AdvancedSearch/Filter"/>'
+    );
+    goto.navigateTiddler("$:/AdvancedSearch");
+  });
 }
 
 // function onMount() {}
 // function onUnmount(state) { }
 
 module.exports = {
-	onUpdate
-}
+  onUpdate,
+};
